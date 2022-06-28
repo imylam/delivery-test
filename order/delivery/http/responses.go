2e6d72a50579c71cf93ece461ceb5679fc22e12c
imylam <imylamdev@gmail.com>
2022-06-28 14:05:45 +0800

    Fix integration test failure
    PlaceOrderResponse.ID should be int instead of array of strings.

--- order/delivery/http/responses.go
+++ order/delivery/http/responses.go
@@ -2,9 +2,9 @@ package http
 
 // PlaceOrderReponse represents the place order reponse body
 type PlaceOrderReponse struct {
-	ID       []string `json:"id"`
-	Distance int      `json:"distance"`
-	Status   string   `json:"status"`
+	ID       int    `json:"id"`
+	Distance int    `json:"distance"`
+	Status   string `json:"status"`
 }
 
 // TakeOrderResponse rrepresents the take order reponse body
